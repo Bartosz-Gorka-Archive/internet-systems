2361cbc7a56f46c4435c63b1c20fbc774b8681be
Bartosz Górka <bartosz.gorka@interia.eu>
2019-03-03 09:33:07 +0100

    feat: Added Exercise A2 - part 6A `echo server`

---
+++ src/main/java/bartoszgorka/TPSIServer.java
@@ -0,0 +1,99 @@
+package bartoszgorka;
+
+import com.cedarsoftware.util.io.JsonWriter;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class TPSIServer {
+    /**
+     * @param args Arguments from user
+     * @throws Exception Potential error - we will see stacktrace
+     */
+    public static void main(String[] args) throws Exception {
+        // Set port
+        int port = 8000;
+
+        // Server will use all possible IP addresses
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        // Contexts - paths
+        server.createContext("/", new RootHandlerStaticFile());
+        server.createContext("/echo", new EchoHandler());
+
+        // Show info and start server
+        System.out.println("Starting server on port: " + port);
+        server.start();
+    }
+
+    /**
+     * Root handler with static index.html file in response
+     */
+    static class RootHandlerStaticFile implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            // Read index.html file
+            byte[] response = Files.readAllBytes(Paths.get("index.html"));
+
+            // Set content type as HTML file with encoding UTF-8
+            exchange.getResponseHeaders().set("Content-Type", "text/html;charset=utf-8");
+
+            // Send response headers - status code 200 (OK) and correct length
+            exchange.sendResponseHeaders(200, response.length);
+
+            // "Write" - send response to client
+            OutputStream os = exchange.getResponseBody();
+            os.write(response);
+            os.close();
+        }
+    }
+
+    /**
+     * 6A - Echo handler. Request headers as response body
+     */
+    static class EchoHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            StringBuilder builder = new StringBuilder("{");
+            String prefix = "";
+
+            // Get request headers and prepare response
+            Headers requestHeaders = exchange.getRequestHeaders();
+            for (Map.Entry<String, List<String>> header : requestHeaders.entrySet()) {
+                builder.append(prefix);
+                prefix = ",";
+
+                // Append structure KEY : VALUE
+                String key = JsonWriter.objectToJson(header.getKey());
+                builder.append(key);
+                builder.append(":");
+
+                String value = JsonWriter.objectToJson(header.getValue().toArray());
+                builder.append(value);
+            }
+            // Append closing } char
+            builder.append("}");
+
+            // Pretty format JSON
+            byte[] jsonResponse = JsonWriter.formatJson(builder.toString()).getBytes();
+
+            // Set content type as JSON
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+
+            // Send response headers - status code 200 (OK) and correct length
+            exchange.sendResponseHeaders(200, jsonResponse.length);
+
+            // "Write" - send response to client
+            OutputStream os = exchange.getResponseBody();
+            os.write(jsonResponse);
+            os.close();
+        }
+    }
+}
