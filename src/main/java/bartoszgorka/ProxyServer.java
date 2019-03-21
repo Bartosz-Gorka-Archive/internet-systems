fb2fd3368b8e30b208c7ea2716ed648344edfd39
Bartosz Górka <bartosz.gorka@interia.eu>
2019-03-21 11:09:43 +0100

    feat: Proxy server (#2)

---
+++ src/main/java/bartoszgorka/ProxyServer.java
@@ -0,0 +1,106 @@
+package bartoszgorka;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import sun.misc.IOUtils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class ProxyServer {
+    /**
+     * @param args Arguments from user
+     * @throws Exception Potential error - we will see stacktrace
+     */
+    public static void main(String[] args) throws Exception {
+        // Set port
+        int port = 8000;
+
+        // Server will use all possible IP addresses
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        // Contexts - paths
+        server.createContext("/", new ProxyServer.RootHandlerContent());
+
+        // Show info and start server
+        System.out.println("Starting server on port: " + port);
+        server.start();
+    }
+
+    /**
+     * Root handler
+     */
+    static class RootHandlerContent implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            try {
+                // We can log each request to Proxy
+                Logger logger = Logger.getLogger("RootHandlerLogger");
+                logger.info(exchange.getRequestMethod() + " " + exchange.getRequestURI().toString());
+
+                // Build connection between Proxy and Server
+                URL url = exchange.getRequestURI().toURL();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // Disable follow redirects
+                connection.setInstanceFollowRedirects(false);
+                HttpURLConnection.setFollowRedirects(false);
+
+                // Set method and headers like in origin request
+                connection.setRequestMethod(exchange.getRequestMethod());
+                for (Map.Entry<String, List<String>> header : exchange.getRequestHeaders().entrySet()) {
+                    connection.addRequestProperty(header.getKey(), String.join(", ", header.getValue()));
+                }
+                System.out.println(connection.getURL());
+
+                for (Map.Entry<String, List<String>> header : connection.getRequestProperties().entrySet()) {
+                    System.out.println(header.getKey());
+                    System.out.println(header.getValue());
+                }
+
+                // Add body from origin request
+                String method = exchange.getRequestMethod().toLowerCase();
+                if (method.equals("post") || method.equals("put") || method.equals("patch")) {
+                    connection.setDoOutput(true);
+                    OutputStream outputStream = connection.getOutputStream();
+                    byte[] bytes = IOUtils.readFully(exchange.getRequestBody(), -1, false);
+                    outputStream.write(bytes);
+                }
+
+                // Make request and build response
+                int code = connection.getResponseCode();
+                byte[] response = {};
+                try {
+                    response = IOUtils.readFully(connection.getInputStream(), -1, false);
+                } catch (Exception e) {
+                    if (connection.getErrorStream() != null) {
+                        response = IOUtils.readFully(connection.getErrorStream(), -1, false);
+                    }
+                }
+
+                // Add headers
+                for (Map.Entry<String, List<String>> header : connection.getHeaderFields().entrySet()) {
+                    if (header.getKey() != null && !header.getKey().toLowerCase().equals("transfer-encoding")) {
+                        exchange.getResponseHeaders().add(header.getKey(), String.join(", ", header.getValue()));
+                    }
+                }
+
+                // Return response
+                exchange.sendResponseHeaders(code, response.length);
+                OutputStream os = exchange.getResponseBody();
+                if (response.length > 0) {
+                    os.write(response);
+                }
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
