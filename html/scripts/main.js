65209b9ff863c99786c4d4955bba2d863d7c71a6
Bartosz Górka <bartosz.gorka@interia.eu>
2019-05-25 10:01:25 +0200

    Added logic on students list

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -5,10 +5,8 @@ function loadStudents(model) {
   $.ajax({
     url: SERVER_URL + '/students',
     type: 'GET',
-    dataType: 'json',
-    accepts: {
-      contentType: 'application/json'
-    }
+    dataType : "json",
+    contentType: "application/json"
   }).done(function(result) {
     ko.mapping.fromJS(result, {}, model.students);
     model.students.subscribe(modifyStudent, null, 'arrayChange');
@@ -16,24 +14,18 @@ function loadStudents(model) {
 }
 
 function modifyStudent(changes) {
-  console.log(changes)
   changes.forEach(function(change) {
     // Student deleted from database
     if (change.status === 'deleted') {
       $.ajax({
         url: resourceUrl(change.value),
         type: 'DELETE',
-        dataType: 'json',
-        contentType: 'application/json',
-        accepts: {
-          contentType: 'application/json'
-        },
+        dataType : "json",
+        contentType: "application/json"
       }).done(function() {
         console.log('Student removed from database');
       });
     }
-    console.log(change)
-    // TODO new
   })
 }
 
@@ -48,29 +40,14 @@ function resourceUrl(record) {
 
 $(document).ready(function(){
   function Student(data) {
-    this.index = ko.observable(data.index);
-    this.firstName = ko.observable(data.firstName);
-    this.lastName = ko.observable(data.lastName);
-    this.dateOfBirth = ko.observable(data.dateOfBirth);
-    this.links = ko.observable(data.links);
-    this.i = '1';
-
-    this.firstName.subscribe(function() {
-      console.log('sdadsadasdas');
-    })
+    var self = this;
+    self.index = ko.observable(data.index);
+    self.firstName = ko.observable(data.firstName);
+    self.lastName = ko.observable(data.lastName);
+    self.dateOfBirth = ko.observable(data.dateOfBirth);
+    self.link = ko.observableArray();
+    ko.mapping.fromJS(data.link, {}, self.link);
   }
-
-  var userMapping = {
-    students: {
-      key: function(item) {
-        return ko.utils.unwrapObservable(item.index);
-      },
-      create: function(options) {
-        console.log('222222')
-        return new Student(options.data);
-      }
-    }
-};
 
   var StateViewModel = function () {
     var self = this;
@@ -82,29 +59,25 @@ $(document).ready(function(){
       dateOfBirth: ko.observable()
     };
     self.removeStudent = function(student) {
-      // self.students.remove(student)
+      self.students.remove(student)
     };
-    self.save = function() {
-      console.log('save');
-      // console.log(self.students())
+    self.saveNewStudent = function() {
+      $.ajax({
+        url: SERVER_URL + '/students',
+        type: 'POST',
+        dataType : "json",
+        contentType: "application/json",
+        data: ko.mapping.toJSON(self.newStudent)
+      }).done(function(data) {
+        self.students.push(new Student(data));
+        self.newStudent.firstName('');
+        self.newStudent.lastName('');
+        self.newStudent.dateOfBirth('');
+      });
     }
-
-    $.ajax({
-      url: SERVER_URL + '/students',
-      type: 'GET',
-      dataType: 'json',
-      accepts: {
-        contentType: 'application/json'
-      }
-    }).done(function(result) {
-      ko.mapping.fromJS(result, {}, self.students);
-    });
-
-    // self.students.subscribe(modifyStudent, null, 'update');
-    // self.students.subscribe(modifyStudent, null, 'arrayChange');
   }
   var model = new StateViewModel();
-  ko.applyBindings(model, );
+  ko.applyBindings(model);
 
-  // loadStudents(model);
+  loadStudents(model);
 });
